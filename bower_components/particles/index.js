4aacfc09cdeaaaf5c54ea08415819767a9227775
NiyeT <niyeturner@gmail.com>
2017-10-29 04:35:51 -0700

    push components

---
+++ bower_components/particles/index.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+  Particles.init({
+    selector: '.particles',
+    color:'#FFFAFA',
+    // color:"#000000",
+    maxParticles:200,
+    sizeVariations:5,
+    speed:0.5,
+    minDistance:120,
+    connectParticles:false,
+  });
+};
